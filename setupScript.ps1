bcebf60bc315ec417fab184365e3edc4f006bb9f
yantex0 <192586328+yantex0@users.noreply.github.com>
2025-04-18 09:32:08 +1000

    Tidied up formatting of setupScript.ps1

--- setupScript.ps1
+++ setupScript.ps1
@@ -1,14 +1,20 @@
-Set-ItemProperty -Path HKCU:\software\microsoft\windows\currentversion\explorer\advanced -Name 'TaskbarAl' -Type 'DWord' -Value 0 #Start Menu to the left
+### Registry settings ###
+
+# Set taskbar alignment to left
+Set-ItemProperty -Path HKCU:\software\microsoft\windows\currentversion\explorer\advanced -Name 'TaskbarAl' -Type 'DWord' -Value 0 
+
+# Disable Windows 11 context menu (Create key + edit key)
 New-Item -Path 'HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32' -Force | Out-Null
 New-ItemProperty -Path 'HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32' -Name '(default)' -Value '' -PropertyType String -Force # Disable Windows 11 context menu
 
 
-# List of packages to install
+
+### Package Installations ###
 
 $packages = @(
 
 	"Microsoft.WindowsTerminal",
-	"Mozilla.Firefox",
+	"Brave.Brave",
 	"Google.GoogleDrive",
 	"7zip.7zip",
 	"Notepad++.Notepad++",
@@ -25,15 +31,10 @@ $packages = @(
 )
 
 # Loop through each package and install it with necessary flags
-
 foreach ($package in $packages) {
-
 	Write-Output "Installing $package..."
-
-	winget install -e --id $package --accept-source-agreements --silent
-
+	winget install -e --id $package --accept-package-agreements --accept-source-agreements --silent
 }
 
 Write-Output "All installations completed!"
 Read-Host -Prompt "Press Enter to exit"
-
